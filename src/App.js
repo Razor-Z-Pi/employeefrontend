6317fbb76c2caec03f4a1cfd78ab98c53febda53
Razor_Z_Pi <popovp.popoff2001@gmail.com>
2022-01-26 12:38:03 +0700

    ver 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, BrowserRouter
+} from "react-router-dom";
+import Home from "./Pages/Home";
+
+function Linker() {
+  return(
+    <Router>
+      <Route path="/Home">
+        <Home />
+      </Route>
+    </Router>
+  );
+}
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Authorization">
+        <section className="modal">
+          <h1>Авторизация</h1>
+          <form method="POST">
+            <div>
+              <label>Логин</label>
+              <input type="text"  placeholder="Логин" />
+            </div>
+            <div>
+              <label>Пароль</label>
+              <input type="text" placeholder="Пароль" />
+            </div>
+            <button type="button" className="" onClick={Linker()}>Войти</button>
+          </form>
+        </section>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
