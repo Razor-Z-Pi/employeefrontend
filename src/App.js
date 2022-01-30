f22c6da288927430a99b08f5effe34e351429fe8
Razor_Z_Pi <popovp.popoff2001@gmail.com>
2022-01-31 01:41:37 +0700

    ver 0.001

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
 import './App.css';
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Component/Header";
 
 function App() {
+
+  const [user, setUser] = useState(""); //значение input
+  const [password, setPassword] = useState("");
+  const [userDirty, setUserDirty] = useState(false); //отражает были мы в input
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [userError, setUserError] = useState("Имя не должно быть пустым"); //Уведомления об о ошибки
+  const [passwordError, setPasswordError] = useState("Пароль не должен быть пустым");
+  const [formValid, setFormValid] = useState(false); // состояние кнопки
+
+  // разблокировка кнопки или нет???
+  useEffect(() => {
+    if (userError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [userError, passwordError]);
+
+  // слушатели на не корректный ввод
+  const userHandler = (e) => {
+    setUser(e.target.value);
+    const sec = /^\S*$/;
+    if (!sec.test(String(e.target.value).toLowerCase())) {
+      setUserError("Не корректный ввод");
+    } else {
+      setUserError("");
+    }
+  }
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+    const sec = /^\S*$/;
+
+    if (e.target.value.length < 8) {
+      setPasswordError("");
+      if (!e.target.value || !sec.test(String(e.target.value).toLowerCase())) {
+        setPasswordError("Не корретный ввод");
+      }
+    } else {
+      setPasswordError("Пароль не больше 8 цифр");
+    }
+  }
+
+  //Когда пользователь покинул поле ввода
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "user":
+        setUserDirty(true);
+        break;
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  }
+
   return (
-    <Header></Header>
+    <div>
+      <form action="">
+        <h1>Авторизация</h1>
+        {
+          (userError && userDirty) && <div style={{color:"red"}}>{userError}</div>
+        }
+        <input onChange={e => userHandler(e)} value={user} onBlur={e => blurHandler(e)} name="user" type="text"/>
+        {
+          (passwordError && passwordDirty) && <div style={{color:"red"}}>{passwordError}</div>
+        }
+        <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name="password" type="password"/>
+        <button disabled={!formValid} type="submit">Войти</button>
+      </form>
+    </div>
   );
 }
 
